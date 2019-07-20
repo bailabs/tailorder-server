d5af83ed6cb3e07a3335346f7b15db5617921725
Ivan Ray Altomera <irayspacii@gmail.com>
2019-07-20 15:33:27 +0800

    fix: create_order_series passed db

--- install_app.py
+++ install_app.py
@@ -6,6 +6,6 @@ app = create_app()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        create_order_series()
+        create_order_series(db)
         print("Finished...")
 
